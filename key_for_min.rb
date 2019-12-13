6d9be8ffeeeb4b32e97ed40a176d9c74d0c889e7
Tammy Lam <lamtammy1515@gmail.com>
2019-12-13 03:19:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,12 @@ def key_for_min_value(name_hash)
    
    return nil if name_hash.size == 0
    
-     min_key, min_value = name_hash.first
+     small_key, small_value = name_hash.first
    name_hash.each do |key, value|
      
       if value < min_value
-      min_key = key
+      small_key = key
+         end
+   end
+   small_key
+end
